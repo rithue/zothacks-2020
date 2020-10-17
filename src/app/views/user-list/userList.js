ce1e03e1c86af08d71998d3424b6f72f0482bdcc
Lasse Nordahl <lasseanordahl@gmail.com>
2020-10-17 11:20:13 -0700

    adjusted template for starting download

--- src/app/views/user-list/userList.js
+++ src/app/views/user-list/userList.js
@@ -1,37 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./userList.scss";
 
-import axios from "axios"
-
-import { UserCard } from "app/components";
-
 function UserList() {
-
-  // List of users from the database
-  const [users, setUsers] = useState(null);
-
-  // Get the list of users from the database upon page load
-  useEffect(() => {
-    getUserList();
-  }, []);
-
-  // Get the list of users through a GET request to the backend API
-  async function getUserList() {
-    let users = await axios.get("http://127.0.0.1:5000/user");
-    
-    // If we get a valid response, set the state object, or print an error.
-    if (users.status === 200) {
-      setUsers(users.data.data);
-    } else {
-      console.log("Error retrieving users");
-    }
-  }
 
   return (
     <div className="user-list">
-      {(users || []).map(function (user, index) {
-        return <UserCard user={user} key={index} number={index}/>;
-      })}
+      Welcome to the ZotHacks starter template!
     </div>
   );
 }
